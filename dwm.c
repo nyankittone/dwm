cd109ce1dd6e52fd2de43d745afc4f374ffb626a
nyankittone <farrisluke8@gmail.com>
2025-03-13 00:29:58 -0400

    Applied cursorwarp patch

--- dwm.c
+++ dwm.c
@@ -899,6 +899,10 @@ focusmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+	if (selmon->sel)
+		XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
+    else
+        XWarpPointer(dpy, None, RootWindow(dpy, screen), 0, 0, 0, 0, selmon->mx + selmon->mw/2, selmon->my + selmon->mh/2);
 }
 
 void
@@ -924,6 +928,7 @@ focusstack(const Arg *arg)
 	if (c) {
 		focus(c);
 		restack(selmon);
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	}
 }
 
